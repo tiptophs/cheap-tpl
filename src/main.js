04e69538a46558441f7f92261fa13ba49cfb7985
tiptophs <1085556180@qq.com>
2020-07-11 14:22:56 +0800

    feat: 修改配置dev-server和output的publicPath导致的run dev的问题

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-//引入vue，vue-router
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
+// 引入vue，vue-router
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 
-//生产环境下关闭提示，阻止生产模式产生的提示信息
-Vue.config.productionTip = false
+// 生产环境下关闭提示，阻止生产模式产生的提示信息
+Vue.config.productionTip = false;
 
-//初始话Vue
+// 初始话Vue
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
 
-
-//模拟http请求，演示proxy
-let xhr = new XMLHttpRequest();
-//请求
+// 模拟http请求，演示proxy
+const xhr = new XMLHttpRequest();
+// 请求
 xhr.open('GET', '/api/user', true);
-//加载之后
-xhr.onload = function(){
+// 加载之后
+xhr.onload = function () {
   console.log(xhr.response);
-}
-//发送
+};
+// 发送
 xhr.send();
-
