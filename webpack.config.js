2d0d3b1fd0dbac900522c9f0caaa25b33ab245b9
tiptophs <1085556180@qq.com>
2020-07-11 13:53:53 +0800

    feat: 添加eslint规则

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',         // 模式，默认两种 production和development
-    entry: './src/index.js',    // 入口，从哪个文件开始打包
-    output: {                   // 出口
-        filename: 'bundle.[hash:5].js',             // 打包后的文件名
-        path: path.resolve(__dirname, './build')   // 路径必须是一个绝对路径
-    },
-    devServer: {                // 开发服务器的配置
-        port: 3000,             //端口
-        progress: true,         // 打包时显示进度条
-        contentBase: './build', // 指定目录做为静态服务
-        compress: true          // 启用gzip压缩
-    },
-    plugins: [  // 数组，放着所有的webpack插件
-        new HtmlWebpackPlugin({
-            template: './src/index.html',       // 指定要打包的模板
-            filename: 'index.html',             // 打包后生成的文件
-            minify: {                           // 压缩操作 
-                removeAttributeQuotes: true,    // 删除模板中的双引号
-                collapseWhitespace: true        // 将模板压缩成一行
-            },
-            hash: true                          // 模板中引用的js文件带哈希戳
-        })
-    ]
+  mode: 'production', // 模式，默认两种 production和development
+  entry: './src/index.js', // 入口，从哪个文件开始打包
+  output: { // 出口
+    filename: 'bundle.[hash:5].js', // 打包后的文件名
+    path: path.resolve(__dirname, './build'), // 路径必须是一个绝对路径
+  },
+  devServer: { // 开发服务器的配置
+    port: 3000, // 端口
+    progress: true, // 打包时显示进度条
+    contentBase: './build', // 指定目录做为静态服务
+    compress: true, // 启用gzip压缩
+  },
+  plugins: [ // 数组，放着所有的webpack插件
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 指定要打包的模板
+      filename: 'index.html', // 打包后生成的文件
+      minify: { // 压缩操作
+        removeAttributeQuotes: true, // 删除模板中的双引号
+        collapseWhitespace: true, // 将模板压缩成一行
+      },
+      hash: true, // 模板中引用的js文件带哈希戳
+    }),
+  ],
 };
